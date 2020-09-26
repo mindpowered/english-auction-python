5cacd535688d8b4e79cbc6db7b637c7f0bec86a4
Build <support@mindpowered.ca>
2020-09-26 22:49:27 +0000

    Version 0.0.11

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
     #long_description = fh.read()
 setuptools.setup(
      name='mindpowered-englishauction',
-     version='0.0.10',
+     version='0.0.11',
      description="EnglishAuction",
      #long_description=long_description,
      #long_description_content_type="text/markdown",
