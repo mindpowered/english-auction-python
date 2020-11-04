ca7addf944a031329fda76ebc3c6141f2e97b65f
Build <support@mindpowered.ca>
2020-11-04 05:06:21 +0000

    Version 0.0.31

--- demowrapper/EnglishAuction.py
+++ demowrapper/EnglishAuction.py
@@ -1,140 +1,226 @@
+import maglev
+import englishauction
+
 class EnglishAuction:
 	"""
 	An Auction Library
 	Timed auction starting at a low price and increasing until no more bids are made.
 	Also supports reserve price and automatic bidding.
 	"""
+	def __init__(self):
+		bus = maglev.maglev_MagLev.getInstance("default")
+		lib = englishauction.englishauction_EnglishAuction(bus)
 
-	def Create(start: int, end: int, startingPrice: int, reservePrice: int, priceIncrement: int) -> bool:
+	def Create(self, start: string, end: string, startingPrice: number, reservePrice: number, priceIncrement: number) -> string:
 		"""		Create a new auction
 		Args:
-			start (int):start time of auction
-			end (int):end time of auction
-			startingPrice (int):starting price of auction
-			reservePrice (int):reserve price for the auction (0 = none)
-			priceIncrement (int):price increments for bids in the auction
+			start (string):start time of auction
+			end (string):end time of auction
+			startingPrice (number):starting price of auction
+			reservePrice (number):reserve price for the auction (0 = none)
+			priceIncrement (number):price increments for bids in the auction
 		Returns:
-			RET HERE
+			auctionId
 		"""
-	def GetStart(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [start, end, startingPrice, reservePrice, priceIncrement]
+		ret = bus.call('EnglishAuction.Create', args);
+		return ret;
+
+	def GetStart(self, auctionId: string) -> number:
 		"""		Get the start of an auction
 		Will return a timestamp in milliseconds
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			start of auction
 		"""
-	def GetEnd(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetStart', args);
+		return ret;
+
+	def GetEnd(self, auctionId: string) -> bool:
 		"""		Check if auction has ended
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			true if auction has ended, false otherwise
 		"""
-	def HasStarted(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetEnd', args);
+		return ret;
+
+	def HasStarted(self, auctionId: string) -> bool:
 		"""		Check if an auction has started yet
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			true if auction started, false otherwise
 		"""
-	def Bid(auctionId: int, userId: int, price: int):
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.HasStarted', args);
+		return ret;
+
+	def Bid(self, auctionId: string, userId: string, price: number):
 		"""		Create a bid in an auction
 		Args:
-			auctionId (int):auction id
-			userId (int):user id
-			price (int):price bud
+			auctionId (string):auction id
+			userId (string):user id
+			price (number):price bud
 		"""
-	def GetHighestBidder(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId, userId, price]
+		bus.call('EnglishAuction.Bid', args);
+
+	def GetHighestBidder(self, auctionId: string) -> mixed:
 		"""		Get the highest bidder in an auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			
 		"""
-	def GetHighestBids(auctionId: int, numBids: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetHighestBidder', args);
+		return ret;
+
+	def GetHighestBids(self, auctionId: string, numBids: number) -> array:
 		"""		Get the highest bids in an auction
 		Args:
-			auctionId (int):auction id
-			numBids (int):max number of highest bids to return
+			auctionId (string):auction id
+			numBids (number):max number of highest bids to return
 		Returns:
-			RET HERE
+			Highest bids for the specified auction
 		"""
-	def GetNumberOfBids(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId, numBids]
+		ret = bus.call('EnglishAuction.GetHighestBids', args);
+		return ret;
+
+	def GetNumberOfBids(self, auctionId: string) -> number:
 		"""		Get the number of bids in an auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			Number of bids placed in the specified auction
 		"""
-	def GetPriceIncrement(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetNumberOfBids', args);
+		return ret;
+
+	def GetPriceIncrement(self, auctionId: string) -> number:
 		"""		Get the price increment for the specified auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			Price increment
 		"""
-	def GetReservePrice(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetPriceIncrement', args);
+		return ret;
+
+	def GetReservePrice(self, auctionId: string) -> number:
 		"""		Get the reserve price for the specified auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			Reserve price
 		"""
-	def GetStartingPrice(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetReservePrice', args);
+		return ret;
+
+	def GetStartingPrice(self, auctionId: string) -> number:
 		"""		Get the starting price for the specified auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			Starting price
 		"""
-	def CalcTimeRemaining(auctionId: int, now: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.GetStartingPrice', args);
+		return ret;
+
+	def CalcTimeRemaining(self, auctionId: string, now: number) -> number:
 		"""		Get the time remaining for the specified auction
 		Args:
-			auctionId (int):auction id
-			now (int):current unix timestamp
+			auctionId (string):auction id
+			now (number):current unix timestamp
 		Returns:
-			RET HERE
+			Time remaining in seconds
 		"""
-	def CalcMinimumBid(auctionId: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId, now]
+		ret = bus.call('EnglishAuction.CalcTimeRemaining', args);
+		return ret;
+
+	def CalcMinimumBid(self, auctionId: string) -> number:
 		"""		Get the minimum next bid for the specified auction
 		Args:
-			auctionId (int):auction id
+			auctionId (string):auction id
 		Returns:
-			RET HERE
+			Minimum bid price
 		"""
-	def GetAuctionsEnding(endfrom: int, endto: int, page: int, perpage: int, sort: int, asc: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [auctionId]
+		ret = bus.call('EnglishAuction.CalcMinimumBid', args);
+		return ret;
+
+	def GetAuctionsEnding(self, endfrom: number, endto: number, page: number, perpage: number, sort: string, asc: bool) -> array:
 		"""		Get a list of auctions based on their end time
 		Args:
-			endfrom (int):end from
-			endto (int):end to
-			page (int):
-			perpage (int):number of auctions per page
-			sort (int):field to sort by
-			asc (int):ascending (true) or descending (false)
+			endfrom (number):end from
+			endto (number):end to
+			page (number):
+			perpage (number):number of auctions per page
+			sort (string):field to sort by
+			asc (bool):ascending (true) or descending (false)
 		Returns:
-			RET HERE
+			List of auctions ending in the specified period
 		"""
-	def GetAuctionsStarting(startfrom: int, startto: int, page: int, perpage: int, sort: int, asc: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [endfrom, endto, page, perpage, sort, asc]
+		ret = bus.call('EnglishAuction.GetAuctionsEnding', args);
+		return ret;
+
+	def GetAuctionsStarting(self, startfrom: number, startto: number, page: number, perpage: number, sort: string, asc: bool) -> array:
 		"""		Get a list of auctions based on their start time
 		Args:
-			startfrom (int):start from
-			startto (int):start to
-			page (int):
-			perpage (int):number of auctions per page
-			sort (int):field to sort by
-			asc (int):ascending (true) or descending (false)
+			startfrom (number):start from
+			startto (number):start to
+			page (number):
+			perpage (number):number of auctions per page
+			sort (string):field to sort by
+			asc (bool):ascending (true) or descending (false)
 		Returns:
-			RET HERE
+			List of auctions starting in the specified period
 		"""
-	def GetOpenAuctions(page: int, perpage: int, sort: int, asc: int) -> bool:
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [startfrom, startto, page, perpage, sort, asc]
+		ret = bus.call('EnglishAuction.GetAuctionsStarting', args);
+		return ret;
+
+	def GetOpenAuctions(self, page: number, perpage: number, sort: string, asc: bool) -> array:
 		"""		Get a list of currently running auctions
 		Args:
-			page (int):
-			perpage (int):number of auctions per page
-			sort (int):field to sort by
-			asc (int):ascending (true) or descending (false)
+			page (number):
+			perpage (number):number of auctions per page
+			sort (string):field to sort by
+			asc (bool):ascending (true) or descending (false)
 		Returns:
-			RET HERE
+			List of open auctions
 		"""
-#END
+		bus = maglev.maglev_MagLev.getInstance("default")
+		args = [page, perpage, sort, asc]
+		ret = bus.call('EnglishAuction.GetOpenAuctions', args);
+		return ret;
+
+
+
--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 import setuptools
 
-#with open("README.md", "r") as fh:
-    #long_description = fh.read()
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setuptools.setup(
-     name='mindpowered-englishauction',
-     version='0.0.30',
-     author="Mind Powered Corporation",
-     author_email="support@mindpowered.dev",
-     license="CPAL-1.0",
-     url="https://mindpowered.dev/",
-     description="EnglishAuction",
-     #long_description=long_description,
-     #long_description_content_type="text/markdown",
-     py_modules=['englishauction'],
-     classifiers=[
-         "Programming Language :: Python :: 3",
-         "Operating System :: OS Independent",
-     ],
+    name='mindpowered-englishauction',
+    version='0.0.31',
+    author="Mind Powered Corporation",
+    author_email="support@mindpowered.dev",
+    license="CPAL-1.0",
+    url="https://mindpowered.dev/",
+    description="EnglishAuction",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    py_modules=['englishauction'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
  )
