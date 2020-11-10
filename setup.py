02d611c56c3f2333bcf902b5efcbcd0cd5be13b6
Build <support@mindpowered.ca>
2020-11-10 23:55:11 +0000

    Version 0.0.42

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
     name='mindpowered-englishauction',
-    version='0.0.41',
+    version='0.0.42',
     author="Mind Powered Corporation",
     author_email="support@mindpowered.dev",
     license="CPAL-1.0",
